549818946e9e9a0ac724ab357323cdac7df6a704
vs0uz4 <vitor.rodrigues@gmail.com>
2019-04-30 00:30:19 -0300

    Rafactored File Controller for Include S3 Storage Upload Feature

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,21 +1,44 @@
+const aws = require('aws-sdk')
+const crypto = require('crypto')
+
 const Box = require('../models/Box')
 const File = require('../models/File')
 
+const config = require('../config')
+const s3Config = require('../config/s3')
+
 class FileController {
-  async store (req, res) {
-    const box = await Box.findById(req.params.id)
+  store (req, res) {
+    const s3ObjectKey = `${crypto.randomBytes(16).toString('hex')}_${req.file.originalname}`
+    const s3Bucket = new aws.S3(s3Config)
 
-    const file = await File.create({
-      title: req.file.originalname,
-      path: req.file.key
+    const params = {
+      Bucket: config.s3.S3_BUCKET_NAME,
+      Key: s3ObjectKey,
+      Body: req.file.buffer,
+      ContentType: req.file.mimetype,
+      ACL: 'public-read'
+    }
+
+    s3Bucket.upload(params, async (err, data) => {
+      if (err) {
+        res.status(500).json({ error: true, message: err })
+      } else {
+        const box = await Box.findById(req.params.id)
+        const file = await File.create({
+          title: req.file.originalname,
+          etag: data.ETag,
+          key: data.Key
+        })
+
+        box.files.push(file)
+        await box.save()
+
+        req.io.sockets.in(box._id).emit('file', file)
+
+        res.json(file)
+      }
     })
-
-    box.files.push(file)
-    await box.save()
-
-    req.io.sockets.in(box._id).emit('file', file)
-
-    return res.json(file)
   }
 }
 
